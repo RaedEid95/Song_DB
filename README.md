2e8514a5163336d3b0085424eaeb0ebf9ac90d88
RaedEid95 <raed.e.alrfou@gmail.com>
2022-07-28 13:31:04 +0300

    Build the ETL

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Song_DB
+
+**Introduction**
+
+There is a company called sparkify having an Application  for live songs and their data on JSON files and they want to transfer the data to Postgres DB 
+
+
+
+**Steps** 
+
+1. Analyze the data source and define the keys
+2. Create a schema that will help to have a high-performance query (Star Schema)
+![UML Schema](UML class.png)
+3. Create DB using shell and user and gain all privileges for the user on the DB
+4. Create a python script that will Create tables based on the UML 
+5. Create an ETL process that will parse the JSON files and Insert the data in the DB
+6. Build an SQL script that will query the data and test the performance 
---
+++ crete_table.py
@@ -0,0 +1,43 @@
+import os
+import psycopg2
+import queries
+from dotenv import load_dotenv
+
+
+
+def create_table(cur,conn):
+    for query in queries.create_table_queries:
+        cur.execute(query)
+        conn.commit()
+        
+
+def drop_table_if_exisit(cur,conn):
+    for query in queries.drop_table_queries:
+        print("hi")
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    load_dotenv()
+    try:
+        conn = psycopg2.connect(
+        host=os.getenv("HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"))
+        print(conn.info)
+    except:
+        raise(f"error in connection")
+
+    cur = conn.cursor()
+
+    
+    drop_table_if_exisit(cur, conn)
+    create_table(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
